16a91e7ae84937221f530d0b091ac653494eb935
allenjiji <allenjiji2604@gmail.com>
2020-08-01 13:44:13 +0530

    part 2 complete,home and about routes set

---
+++ blog/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def home(request):
+    return HttpResponse('<h1>This is Home Page of Blog</h1>') 
+
+def about(request):
+    return HttpResponse('<h1>This is About Page of Blog</h1>') 
