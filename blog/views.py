e561cf4f7f815c428bb719f5bc54a74a6705934f
allenjiji <allenjiji2604@gmail.com>
2020-08-03 10:14:14 +0530

    modified views.py

--- blog/views.py
+++ blog/views.py
@@ -6,4 +6,4 @@ def home(request):
     return HttpResponse('<h1>This is Home Page of Blog</h1>') 
 
 def about(request):
-    return HttpResponse('<h1>This is About Page of Blog</h1>') 
+    return HttpResponse('<h1>This is About Page of Blog</h1>')
