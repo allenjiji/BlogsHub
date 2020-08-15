4fe101e9faa51915bfd676ff93ddbf857581c649
allenjiji <allenjiji2604@gmail.com>
2020-08-15 19:45:47 +0530

    done with stage 5

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Post
 # Create your views here.
 
+
 def home(request):
-    return HttpResponse('<h1>This is Home Page of Blog</h1>') 
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'blog/home.html', context)
+
 
 def about(request):
-    return HttpResponse('<h1>This is About Page of Blog</h1>')
+    return render(request, 'blog/about.html', {'title': 'New About Page'})
