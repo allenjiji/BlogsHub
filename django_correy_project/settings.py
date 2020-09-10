16aa24d442de956a9b80acfb354114b4943aaefe
allenjiji <allenjiji2604@gmail.com>
2020-09-10 15:04:48 +0530

    after collect static

--- django_correy_project/settings.py
+++ django_correy_project/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -134,5 +133,3 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGOUT_REDIRECT_URL = 'login'
 LOGIN_URL = 'login'
-
-django_heroku.settings(locals())
